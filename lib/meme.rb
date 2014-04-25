16fcd8cb4b20ec497df30905cf9a47ecedc57c9e
Ethan Burrow <ethan@bypassmobile.com>
2014-04-25 13:38:51 -0500

    preliminary refactor to move generators to their own file

--- lib/meme.rb
+++ lib/meme.rb
@@ -3,6 +3,7 @@
   require 'nokogiri'
   require 'cgi'
   require 'meme/version'
+  require 'meme/generators'
 
   ##
   # Generate memes using http://memegenerator.net
@@ -21,105 +22,18 @@ module Meme
     USER_AGENT = "meme/#{::Meme::VERSION} Ruby/#{RUBY_VERSION}"
 
     ##
-    # We have some generators up-in-here
-
-    GENERATORS = Hash.new do |_, k|
-      raise Error, "unknown generator #{k}"
-    end
-
-    ##
-    # For creating advice-dog type meme images.
-    #
-    # These can accept up to two lines of text
-
-    def self.advice_dog name, id, generator_id, template_name, first_line = nil
-      template = [id, 'AdviceDogSpinoff', template_name, generator_id, first_line]
-      template.compact
-
-      GENERATORS[name] = template
-    end
-
-    ##
-    # For creating vertical type meme images
-    #
-    # These can accept multiple lines of text
-
-    def self.vertical name, id, template_name
-      GENERATORS[name] = [id, 'Vertical', template_name]
-    end
-
-    # keep generators in alphabetical order
-    # advice_dog 'ANTEATER',           41191,   'anteater'
-    # advice_dog 'A_DODSON',           106375,  'Antoine-Dodson'
-    # advice_dog 'A_DOG',              940,     'Advice-Dog'
-    # advice_dog 'A_FATHER',           1436,    'High-Expectations-Asian-Father'
-    # advice_dog 'ARNOLD',             1236,    'Angry-Arnold'
-    # advice_dog 'BEAR-GRYLLS',        89714,   'Bear-Grylls'
-    # advice_dog 'BUTTHURT_DWELLER',   1438,    'Butthurt-Dweller'
-    # advice_dog 'B_FROG',             1211,    'Foul-Bachelorette-Frog'
-    # advice_dog 'B_FROG2',            1045,    'Foul-Bachelor-Frog'
-    # advice_dog 'BEIBER',             11809,   'Justin-Beiber'
-    # advice_dog 'CATHY',              622381,  'AckCathy'
-    # advice_dog 'CHALLENGE_ACCEPTED', 275025,  'Challenge-Accepted'
-    # advice_dog 'COOL_STORY_HOUSE',   16948,   'cool-story-bro-house'
-    # advice_dog 'CREEPER',            173501,  'Minecraft-Creeper'
-    # advice_dog 'C_WOLF',             931,     'Courage-Wolf'
-    # advice_dog 'C_LEMUR',            1227,    'Chill-Out-Lemur'
-    advice_dog 'DR_EVIL_MEME',       4265606,   958983,  'Dr-Evil-Meme'
-    # advice_dog 'F_FRY',              84688,   'Futurama-Fry'
-    # advice_dog 'G_GRANDPA',          185650,  'Grumpy-Grandpa'
-    # advice_dog 'H_MERMAID',          405224,  'Hipster-Mermaid'
-    # advice_dog 'I_DONT_ALWAYS',      38926,   'The-Most-Interesting-Man-in-the-World'
-    # advice_dog 'I_WOLF',             926,     'Insanity-Wolf'
-    # advice_dog 'JESUS',              1281,    'jesus-says'
-    # advice_dog 'J_DUCREUX',          1356,    'Joseph-Ducreux'
-    # advice_dog 'KANYE',              622033,  'kanyee'
-    # advice_dog 'KEANU',              47718,   'Keanu-reeves'
-    # advice_dog 'KJI',                5840,    'Kim-Jong-IL'
-    # advice_dog 'MILHOUSE',           228797,  'Millhouse'
-    # advice_dog 'MINECRAFT',          122309,  'Minecraft'
-    # advice_dog 'MORE_BEAR',          33675,   'More-Bear'
-    # advice_dog 'O-RLY-OWL',          117041,  'O-RLY-OWL', 'ORLY???'
-    # advice_dog 'OBAMA',              1332,    'Obama-'
-    # advice_dog 'PHILOSORAPTOR',      984,     'Philosoraptor'
-    # advice_dog 'P_OAK',              24321,   'Professor-Oak'
-    # advice_dog 'P_OBAMA',            45557,   'Pissed-off-Obama'
-    # advice_dog 'R_BLACK',            547955,  'Rebecca-Black-Meme'
-    # advice_dog 'REDSHIRT',           239371,  'red-shirt-guy'
-    # advice_dog 'SCUMBAG',            364688,  'Scumbag-Steve'
-    # advice_dog 'SERIOUS_FISH',       7054219, 'Serious-fish-spongebob'
-    # advice_dog 'SHEEN',              488762,  'Charlie-Sheen'
-    # advice_dog 'SNOB',               2994,    'Snob'
-    # advice_dog 'SPARTA',             1013,    'sparta'
-    # advice_dog 'SPIDERMAN',          1037,    'Question-Spiderman'
-    # advice_dog 'SPILLMAN',           622133,  'Sassy-Spillman'
-    # advice_dog 'SWEDISH_CHEF',       186651,  'Swedish-Chef'
-    # advice_dog 'S_AWKWARD_PENGUIN',  983,     'Socially-Awkward-Penguin'
-    # advice_dog 'TOWNCRIER',          434537,  'Towncrier'
-    # advice_dog 'TROLLFACE',          1030,    'Troll-Face'
-    # advice_dog 'UNICORN_BOY',        57022,   'unicorn-boy'
-    # advice_dog 'US_POINT',           131083,  'Uncle-Sam-Point', 'I WANT YOU'
-    # advice_dog 'V_BABY',             11140,   'Victory-Baby'
-    # advice_dog 'XZIBIT',             3114,    'XZIBIT'
-    advice_dog 'Y_U_NO',             166088,    2,        'Y-U-NO', 'Y U NO'
-    # advice_dog 'YODA',               963,     'Advice-Yoda-Gives'
-
-    vertical 'BATMAN',    148359, 'batman-panal-ryan'
-    vertical 'INCEPTION', 107949, 'Inception'
-    vertical 'NEO',       173419, 'Neo'
-    vertical 'THE_ROCK',  417195, 'The-Rock-driving'
-
-    # keep generators in alphabetical order
-
-    ##
     # Looks up generator name
 
     def GENERATORS.match(name)
       # TODO  meme Y U NO DEMETAPHONE?
-      return self[name] if has_key? name
+      return [name,self[name]] if has_key? name
       matcher = Regexp.new(name, Regexp::IGNORECASE)
       _, generator = find { |k,v| matcher =~ k || v.grep(matcher).any? }
       generator || self[name] # raises the error if generator is nil
+    end
+
+    def parse(name, attributes)
+      [attributes[:imageID], name, attributes[:generatorID], attributes[:default]]
     end
 
     ##
@@ -129,9 +43,9 @@ module Meme
       generator = ARGV.shift
 
       if generator == '--list' then
-        width = GENERATORS.keys.map { |command| command.length }.max
+        width = Meme::GENERATORS.keys.map { |command| command.length }.max
 
-        GENERATORS.sort.each do |command, (id, name, _)|
+        Meme::GENERATORS.sort.each do |command, (id, name, _)|
           puts "%-*s  %s" % [width, command, name]
         end
 
@@ -173,7 +87,7 @@ module Meme
     # Generates links for +generator+
 
     def initialize generator
-      @template_id, @template_type, @generator_name, @generator_id, @default_line = GENERATORS.match generator
+      @template_id, @generator_name, @generator_id, @default_line = parse(generator, Meme::GENERATORS[generator])
     end
 
     ##
@@ -204,9 +118,6 @@ module Meme
         post = Net::HTTP::Post.new url.path
         post['User-Agent'] = USER_AGENT
         post.set_form_data post_data
-
-        puts post.path
-        puts post.body
 
         res = http.request post
         location = res['Location']
---
+++ lib/meme/generators.rb
@@ -0,0 +1,10 @@
+module Meme
+  GENERATORS = {
+    'All-The-Things'   => { :imageID => 1985197, :generatorID => 318065 },
+    'Dr-Evil-Meme'     => { :imageID => 4265606, :generatorID => 958983 },
+    'Disaster-Girl'    => { :imageID => 1232401, :generatorID => 36240 },
+    'Futurama-Fry'     => { :imageID => 84688, :generatorID => 305 },
+    'Philosoraptor'    => { :imageID => 984, :generatorID => 17 },
+    'Y-U-NO'           => { :imageID => 166088, :generatorID => 2, :default => "Y U NO" },
+  }
+end
