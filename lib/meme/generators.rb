2fb804e8c0adac51ba229028609bd7ea6e31eac5
Ethan Burrow <ethan@bypassmobile.com>
2014-04-25 13:49:00 -0500

    add a couple more memes

--- lib/meme/generators.rb
+++ lib/meme/generators.rb
@@ -4,7 +4,9 @@ module Meme
     'Dr-Evil-Meme'     => { :imageID => 4265606, :generatorID => 958983 },
     'Disaster-Girl'    => { :imageID => 1232401, :generatorID => 36240 },
     'Futurama-Fry'     => { :imageID => 84688, :generatorID => 305 },
+    'Grumpy-Cat'       => { :imageID => 6541210, :generatorID => 1771888},
     'Philosoraptor'    => { :imageID => 984, :generatorID => 17 },
+    'Scumbag-Steve'    => { :imageID => 366130, :generatorID => 142},
     'Y-U-NO'           => { :imageID => 166088, :generatorID => 2, :default => "Y U NO" },
   }
 end
