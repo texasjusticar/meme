c5078334d8529d9024967b35046c954f9f06d686
Ethan Burrow <ethan@bypassmobile.com>
2014-12-30 23:42:56 -0600

    add cocaine bear meme and reorg memes alpha

--- lib/meme/generators.rb
+++ lib/meme/generators.rb
@@ -1,17 +1,18 @@
 module Meme
   GENERATORS = {
-    'All-The-Things'   => { :imageID => 1985197, :generatorID => 318065 },
+    'All-The-Things'   => { :imageID => 1985197, :generatorID => 318065 }
+    'Aand-Its-Gone'    => { :imageID => 5115616, :generatorID => 1209603},
+    'Cocaine-Bear'     => { :imageID => 192738, :generatorID => 1396},
+    'Doge'             => { :imageID => 9872337, :generatorID => 2454136},
     'Dr-Evil-Meme'     => { :imageID => 4265606, :generatorID => 958983 },
     'Disaster-Girl'    => { :imageID => 1232401, :generatorID => 36240 },
+    'Ermahgerd'        => { :imageID => 4967234, :generatorID => 1166512, :default => "Ermahgerd"},
     'Futurama-Fry'     => { :imageID => 84688, :generatorID => 305 },
     'Grumpy-Cat'       => { :imageID => 6541210, :generatorID => 1771888},
+    'Its-A-Trap'       => { :imageID => 11194792, :generatorID => 3166847 },
     'Philosoraptor'    => { :imageID => 984, :generatorID => 17 },
-    'Scumbag-Steve'    => { :imageID => 366130, :generatorID => 142},
-    'Y-U-NO'           => { :imageID => 166088, :generatorID => 2, :default => "Y U NO" },
-    'Ermahgerd'        => { :imageID => 4967234, :generatorID => 1166512, :default => "Ermahgerd"},
-    'Aand-Its-Gone'    => { :imageID => 5115616, :generatorID => 1209603},
-    'Doge'             => { :imageID => 9872337, :generatorID => 2454136},
     'Pleaseguy'        => { :imageID => 3219856, :generatorID => 668529, :default => "Please"},
-    'Its-A-Trap'       => { :imageID => 11194792, :generatorID => 3166847 }
+    'Scumbag-Steve'    => { :imageID => 366130, :generatorID => 142},
+    'Y-U-NO'           => { :imageID => 166088, :generatorID => 2, :default => "Y U NO" }
   }
 end
